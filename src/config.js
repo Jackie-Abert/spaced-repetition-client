a5911b050e22dea504757b349ba11b18bc7cf11f
Jackie Abert <jax.abert@gmail.com>
2020-11-10 13:43:46 -0500

    server link fixes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
+
 export default {
-  API_ENDPOINT: 'https://shielded-garden-98696.herokuapp.com/api',
+  API_ENDPOINT: 'https://spaced-repetition-jmh.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
 
